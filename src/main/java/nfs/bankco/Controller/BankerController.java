86e8175efb704e4d61db08843712024613d1784e
Qrab <lucasbarq@free.fr>
2022-06-22 15:01:22 +0200

    Changes by sahel

--- src/main/java/nfs/bankco/Controller/BankerController.java
+++ src/main/java/nfs/bankco/Controller/BankerController.java
@@ -4,6 +4,7 @@ package nfs.bankco.Controller;
 import nfs.bankco.Entity.Banker;
 import nfs.bankco.Entity.Customer;
 import nfs.bankco.Repo.BankerRepository;
+import nfs.bankco.Repo.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,17 +13,27 @@ import java.util.List;
 @RestController
 @RequestMapping("/banker")
 public class BankerController {
+
     @Autowired
     private BankerRepository bankerRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
 
 
     @RequestMapping(value = "/test", produces = "application/json")
     public String Test(){
         return "Hello";
     }
+
     @RequestMapping(value = "/customers", produces = "application/json")
     public List<Customer> getCustomersWallet(){
         return bankerRepository.getCustomerWallet(2);
+    }
+
+    @RequestMapping(value = "/update", produces = "application/json")
+    public Customer update(@PathVariable(value="id") final String id){
+        Customer customer = new Customer();
+        return customerRepository.update(customer);
     }
 
 }
